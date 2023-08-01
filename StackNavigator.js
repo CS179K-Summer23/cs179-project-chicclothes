4260e1df59ada39c1cdd3be122ba29e29e3df29e
rsori013 <rsori013@ucr.edu>
2023-08-01 02:46:56 -0700

    set up screens after loging in

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Import your screens when you have them  ~~Declare it here guys...
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 
 const Stack = createNativeStackNavigator();
@@ -21,7 +22,42 @@ const StackNavigator = () => {
 					options={{
 						headerTintColor: "grey",
 					}}
+			/>
+            <Stack.Screen
+					name="Register"
+					component={RegisterScreen}
+					options={{
+						headerTintColor: "grey",
+					}}
 				/>
+				{/* <Stack.Screen
+					name="Gender"
+					component={GenderScreen}
+					options={{
+						headerTintColor: "#grey",
+					}}
+				/>
+				<Stack.Screen
+					name="Age"
+					component={AgeScreen}
+					options={{
+						headerTintColor: "grey",
+					}}
+				/>
+				<Stack.Screen
+					name="Height"
+					component={HeightScreen}
+					options={{
+						headerTintColor: "grey",
+					}}
+				/>
+				<Stack.Screen
+					name="Weight"
+					component={WeightScreen}
+					options={{
+						headerTintColor: "grey",
+					}}
+				/> */}
         </Stack.Navigator>
     );
 };
