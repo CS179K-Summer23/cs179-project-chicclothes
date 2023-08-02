2fc2e6ee5cf735c22b62d750752b7125c3c8f856
jjdelcid <jgdelcid00@gmail.com>
2023-08-01 22:22:35 -0700

    added new login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,25 +1,130 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const LoginScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Welcome to Login Screen</Text>
-        </View>
-    );
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View style={styles.container}>
+    <Image style={styles.image} source={require("./images/Clique_logo.png")} resizeMode={"contain"}/>
+    <View style={styles.inputContainer}>
+					<Image
+						source={require("./images/profilepic.jpeg")}
+						style={styles.icon}
+					/>
+					<TextInput
+						style={[styles.input]}
+						placeholder="Enter Username"
+                        placeholderTextColor="#003f5c"
+                        secureTextEntry={false}
+                        onChangeText={(password) => setPassword(password)}
+					/>
+				</View>
+                <View style={styles.inputContainer}>
+					<Image
+						source={require("./images/passwordpic.png")}
+						style={styles.icon}
+					/>
+					<TextInput
+						style={[styles.input]}
+						placeholder="Enter Password"
+                        placeholderTextColor="#003f5c"
+                        secureTextEntry={true}
+                        onChangeText={(password) => setPassword(password)}
+					/>
+				</View>
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={{ color: "black", fontWeight: "bold" }}>LOGIN</Text> 
+      </TouchableOpacity>
+      <TouchableOpacity
+					style={styles.registerButton}
+					onPress={() => navigation.navigate("Register")}
+				>
+					<Text style={styles.registerButtonText}>
+						Don't have an account?{" "}
+						<Text style={{ color: "black", fontWeight: "bold" }}>
+							Click here
+						</Text>
+					</Text>
+				</TouchableOpacity> 
+    </View> 
+  );
 }
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f5f5f5'
-    },
-    text: {
-        fontSize: 18,
-        color: '#333',
-    }
-});
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    marginBottom: 80,
+    width: 335,
+    height: 135,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+},
+icon: {
+    width: 35,
+    height: 35,
+    marginRight: 15,
+    marginBottom: 14,
+},
+input: {
+    justifyContent: "center",
+    width: "65%",
+    height: 50,
 
-export default LoginScreen;
+    paddingHorizontal: 1,
+    marginBottom: 23,
+
+    fontSize: 16,
+    borderBottomWidth: 2,
+    borderBottomColor: "#ccc",
+    marginRight: 44,
+},
+  inputView: {
+    backgroundColor: "#F5F5DC",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+ 
+  loginButton: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#F5F5DC",
+  },
+  registerButton: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+},
+registerButtonText: {
+    color: "black",
+    fontSize: 16,
+},
+});
