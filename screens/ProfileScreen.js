3f667e88ff3e26610b07d185228edcc35a31a9d3
rsori013 <rsori013@ucr.edu>
2023-08-03 10:50:25 -0700

    added more settings buttons

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 import { View, Text, StyleSheet, Modal, Button, Image, ScrollView, TouchableOpacity } from 'react-native';
 import { AntDesign } from "@expo/vector-icons";
 
 const ProfileScreen = () => {
     const [modalVisible, setModalVisible] = useState(false);
     const [activeModal, setActiveModal] = useState('');
+    const scrollViewRef = useRef(null); //for setting button
+    const sectionRef = useRef(null); //for setting range to show when press
+
+
+
 
     const offers = [
         { image: require('./images/offer1.png'), title: "Up to 25% off select styles", description: "MEMBER PRICES \n Valid until: 12/31/2030 11:59 pm" },
@@ -27,21 +32,78 @@ const ProfileScreen = () => {
     const OptionButton = ({ title, icon }) => {
         return (
             <TouchableOpacity style={styles.optionButton} onPress={() => { setActiveModal(title); setModalVisible(true); }}>
-                <AntDesign name={icon} size={24} color="black" />
-                <Text style={styles.optionButtonText}>{title}</Text>
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <AntDesign name={icon} size={24} color="black" />
+                        <Text style={styles.optionButtonText}>{title}</Text>
+                    </View>
+                    <AntDesign name="right" size={20} color="grey" />
+                </View>
             </TouchableOpacity>
         );
     }
+    
+
+    const renderModalContent = () => {
+        switch (activeModal) {
+            case 'View Member ID':
+                return (
+                <>
+                    <Image source={require('./images/qrcode.png')} style={styles.modalImage} />
+                    <Text style={styles.modalText}>Your QRCODE details go here.</Text>
+                </>
+            );
+            case 'My orders':
+                return <Text style={styles.modalText}>Your orders list goes here.</Text>;
+            case 'Payments':
+                return <Text style={styles.modalText}>Your payment details go here.</Text>;
+            case 'Account Settings':
+                return <Text style={styles.modalText}>Your account settings go here.</Text>;
+            case 'My points':
+                return <Text style={styles.modalText}>Your points summary goes here.</Text>;
+            case 'Membership':
+                return <Text style={styles.modalText}>Your membership details go here.</Text>;
+            default:
+                return null;
+        }
+    };
+
+    const handleSettingsPress = () => {
+        sectionRef.current.measure((x, y, width, height, pageX, pageY) => {
+            scrollViewRef.current?.scrollTo({ x: 0, y: pageY, animated: true });
+        });
+    }
+    
+    
 
     return (
-        <ScrollView style={styles.mainScrollView}>
+        <ScrollView ref={scrollViewRef} style={styles.mainScrollView}>
             <View style={styles.container}>
+                
+            <View style={styles.welcomeContainer}>
+                <Text style={styles.welcomeUser}>Hi User!</Text>
+                <TouchableOpacity onPress={handleSettingsPress}>
+                    <AntDesign name="setting" size={35} color="black" />
+                </TouchableOpacity>
+            </View>
 
-                <View style={styles.rewardContainer}>
+            <View style={styles.rewardContainer}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
                     <Text style={styles.rewardText}>0 points</Text>
-                    <View style={styles.pointTracer}></View>
-                    <Button title="View Member ID" onPress={() => setModalVisible(true)} style={styles.memberIdButton} />
+                    <Button
+                    title="points history"
+                    onPress={() => { /* your function to show modal */ }}
+                    color="black" // Set button text color to black
+                    />
                 </View>
+                <View style={styles.pointTracer}></View>
+                <Text style={styles.greyText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+                <TouchableOpacity style={styles.memberIdButton} onPress={() => { setActiveModal('View Member ID'); setModalVisible(true); }}>
+                    <AntDesign name="qrcode" size={20} color="black" />
+                    <Text style={{ color: 'black', marginLeft: 10 }}>View Member ID</Text>
+                </TouchableOpacity>
+            </View>
+
 
                 <Text style={styles.offersTitle}>My Offers</Text>
                 <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.offerScrollView}>
@@ -53,8 +115,9 @@ const ProfileScreen = () => {
                         </View>
                     ))}
                 </ScrollView>
-
+                <View ref={sectionRef}>
                 {options.map(option => <OptionButton key={option.title} title={option.title} icon={option.icon} />)}
+                </View>
 
                 <Modal
                     animationType="slide"
@@ -66,7 +129,7 @@ const ProfileScreen = () => {
                 >
                     <View style={styles.centeredView}>
                         <View style={styles.modalView}>
-                            <Text style={styles.modalText}>This is the {activeModal} modal.</Text>
+                        {renderModalContent()}
                             <Button
                                 title="Close"
                                 onPress={() => setModalVisible(false)}
@@ -88,14 +151,29 @@ const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
         paddingTop: 5,
-        paddingBottom: 20
+        paddingBottom: 20,
+        backgroundColor: '#ffffff',
+    },
+    welcomeContainer: {
+        flexDirection: 'row',
+        alignItems: 'center', // To align items vertically in the center
+        justifyContent: 'space-between', // To push the items to either end
+        width: '100%', // Take the full width of the parent
+        paddingHorizontal: 15, // A little bit of padding to avoid touching the screen edges
+        paddingTop:15,
+        paddingBottom:10,
+        backgroundColor: '#ffffff',
+    },
+    welcomeUser: {
+        fontSize: 30,
+        fontWeight: 'bold',
     },
     rewardContainer: {
         width: '100%',
         height: 200,
         backgroundColor: '#f0ebdf',
         padding: 20,
-        borderRadius: 5,
+        //borderRadius: 5,
         marginBottom: 20,
         justifyContent: 'space-between',
         alignItems: 'flex-start',
@@ -103,18 +181,39 @@ const styles = StyleSheet.create({
     rewardText: {
         fontSize: 24,
         color: '#000',
-        alignSelf: 'flex-start'  // To align the "0 points" text to the left
+        borderBottomWidth: 1,
+        borderColor: 'black'
+    },
+    greyText: {
+        color: 'grey',
+        fontSize: 12,
+        marginBottom: 5,
     },
     memberIdButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
         borderColor: 'black',
         borderWidth: 1,
         borderRadius: 5,
+        padding: 10,
+        backgroundColor: '#f0ebdf',
+        width: '80%',
         alignSelf: 'center'
     },
+    pointTracer: {
+        height: 3,
+        width: '100%',
+        backgroundColor: 'grey',
+        marginVertical: 10, // Spacing above and below the line
+        // Add other styles for the point tracker marks if needed
+    },
     offersTitle: {
+        alignSelf: 'flex-start', // Add this to override the parent's alignItems
         fontSize: 18,
         fontWeight: 'bold',
-        textAlign: 'left',
+        marginLeft: 20,  // To give some space from the left edge
+        marginTop: 20,
     },
     offerScrollView: {
         paddingLeft: 20,
@@ -144,13 +243,53 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginVertical: 10,
-        padding: 10,
+        paddingHorizontal: 15,   // Add padding to the left and right
+        width: '90%',            // Set the width to 90%
+        justifyContent: 'space-between',  // Distribute items (icon, text, arrow) equally
+        backgroundColor: '#f2f2f2',
         borderRadius: 5,
-        backgroundColor: '#f2f2f2'
+        alignSelf: 'center'  // Center the button within its parent
     },
     optionButtonText: {
-        marginLeft: 10
+        marginLeft: 10,
+        flex: 1,  // Allow the text to take up available space
+        textAlign: 'left'  // Align text to the left
     },
+    modalView: {
+    width: '80%', // Use 80% of the screen width
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+},
+modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    fontSize: 16
+},
+centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+    backgroundColor: 'rgba(0,0,0,0.5)', // semi-transparent background to act as an overlay
+},
+ modalImage: {
+        width: 200,  // adjust based on your needs
+        height: 200, // adjust based on your needs
+        marginBottom: 20,
+    },
+
+
 });
 
 export default ProfileScreen;
