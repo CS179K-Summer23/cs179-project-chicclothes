342bed3f8e68971d4d45ac04bd5448179169be05
jjdelcid <jgdelcid00@gmail.com>
2023-08-06 16:01:16 -0700

    added api

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Welcome to  HomeScreen</Text>
-        </View>
-    );
-}
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+    <View style={{ padding: 10 }}>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={{ marginBottom: 20 }}>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: 100, height: 100 }}
+            />
+            <Text>{item.title}</Text>
+            <Text>{item.price} USD</Text>
+          </View>
+        )}
+        numColumns={2}
+      />
+    </View>
+    </View>
+  );
+}
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f5f5f5'
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
     },
-    text: {
-        fontSize: 18,
-        color: '#333',
-    }
 });
 
 export default HomeScreen;
