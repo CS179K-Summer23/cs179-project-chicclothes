35f7d4362f084a0613e7ab0e3fb1c856f8b9a2c9
rsori013 <rsori013@ucr.edu>
2023-08-05 23:32:59 -0700

    still not working

--- screens/RandomFeatureScreen.js
+++ screens/RandomFeatureScreen.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
-import { GiftedChat } from 'react-native-gifted-chat';
-import axios from 'axios';
+import { GiftedChat } from "react-native-gifted-chat";
+import axios from "axios";
 
 const RandomFeatureScreen = () => {
   const [messages, setMessages] = useState([]);
@@ -10,50 +10,62 @@ const RandomFeatureScreen = () => {
     setMessages([
       {
         _id: 1,
-        text: 'Hello! Ask me anything!',
+        text: "Hello! Ask me anything!",
         createdAt: new Date(),
         user: {
           _id: 2,
-          name: 'ChatGPT',
+          name: "ChatGPT",
         },
       },
-    ])
-  }, [])
+    ]);
+  }, []);
 
   // Function to handle when a user sends a message
   const onSend = (newMessages = []) => {
     setMessages(GiftedChat.append(messages, newMessages));
 
     // Send user's message to the GPT-3 model and get the model's response
-    axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-      "prompt": newMessages[0].text,
-      "max_tokens": 60
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer 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`
-      }
-    })
-    .then(response => {
-      // Append the GPT-3 model's response to the message list
-      setMessages(previousMessages => GiftedChat.append(previousMessages, {
-        _id: previousMessages.length + 1,
-        text: response.data.choices[0].text.trim(),
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'ChatGPT',
+    axios
+      .post(
+        "https://api.openai.com/v1/engines/davinci-codex/completions",
+        {
+          messages: [
+            { role: "system", content: "You are a helpful assistant." },
+            { role: "user", content: newMessages[0].text },
+          ],
         },
-      }))
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  }
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer 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`,
+          },
+        }
+      )
+      .then((response) => {
+        // Append the GPT-3 model's response to the message list
+        setMessages((previousMessages) =>
+          GiftedChat.append(previousMessages, {
+            _id: previousMessages.length + 1,
+            text: response.data["messages"][
+              response.data["messages"].length - 1
+            ]["content"].trim(),
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: "ChatGPT",
+            },
+          })
+        );
+      });
+  };
 
   return (
     <View style={styles.container}>
-      <GiftedChat messages={messages} onSend={newMessages => onSend(newMessages)} user={{ _id: 1 }} />
+      <GiftedChat
+        messages={messages}
+        onSend={(newMessages) => onSend(newMessages)}
+        user={{ _id: 1 }}
+      />
     </View>
   );
 };
