0fe740b0948ca9b11832f7448a78718122dacba5
rsori013 <rsori013@ucr.edu>
2023-08-05 23:09:43 -0700

    created and trying the API key and its implemntation

--- screens/RandomFeatureScreen.js
+++ screens/RandomFeatureScreen.js
@@ -20,7 +20,36 @@ const RandomFeatureScreen = () => {
     ])
   }, [])
 
-  
+  // Function to handle when a user sends a message
+  const onSend = (newMessages = []) => {
+    setMessages(GiftedChat.append(messages, newMessages));
+
+    // Send user's message to the GPT-3 model and get the model's response
+    axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      "prompt": newMessages[0].text,
+      "max_tokens": 60
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer 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`
+      }
+    })
+    .then(response => {
+      // Append the GPT-3 model's response to the message list
+      setMessages(previousMessages => GiftedChat.append(previousMessages, {
+        _id: previousMessages.length + 1,
+        text: response.data.choices[0].text.trim(),
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'ChatGPT',
+        },
+      }))
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
 
   return (
     <View style={styles.container}>
