b6964e31f1dea691ce7f0f12a8fc68c7acb2bad3
rsori013 <rsori013@ucr.edu>
2023-08-09 11:49:16 -0700

    View Member Id is done

--- screens/Profile/ViewMemIdModalContent.js
+++ screens/Profile/ViewMemIdModalContent.js
@@ -4,13 +4,25 @@ import { View, Image,Text, StyleSheet } from "react-native";
 const MemberIDScreen = () => {
   return (
     <View style={styles.container}>
-      
+      <Image source ={require('../images/qrcode.png')} style= {styles.image} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#ffffff",
+  },
+  image: {
+    width: "85%",
+    height: "55%",
+    marginBottom: 250,
+  },
+
 });
 
 export default MemberIDScreen;
