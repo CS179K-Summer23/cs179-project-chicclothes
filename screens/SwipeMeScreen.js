b989ccf959a20d45431993921c38fc0adae80f39
lucyann <llucy.ann514@gmail.com>
2023-08-13 17:00:27 -0700

    Changed up the SwipeMeScreen

--- screens/SwipeMeScreen.js
+++ screens/SwipeMeScreen.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
+import Swiper from 'react-native-deck-swiper';
 import {View, Text, StyleSheet, ScrollView, TouchableOpacity, Image} from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+// import { AntDesign } from "@expo/vector-icons";
 
 const SwipeScreen = () => {
   const [products, setProducts] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [favorites, setFavorites] = useState([]);
+  // const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -15,94 +17,108 @@ const SwipeScreen = () => {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
-  const filterProductsByCategory = (category) => {
-    switch (category) {
-      case 'shirts':
-        return products.filter(product => product.title.toLowerCase().includes('shirt') || product.title.toLowerCase().includes('short sleeve'));
-      case 'pants':
-        return products.filter(product => product.title.toLowerCase().includes('pant') || product.title.toLowerCase().includes('jean'));
-      default:
-        return products.filter(product => 
-          !product.title.toLowerCase().includes('shirt') &&
-          !product.title.toLowerCase().includes('short sleeve') &&
-          !product.title.toLowerCase().includes('pant') &&
-          !product.title.toLowerCase().includes('jean')
-        );
-    }
+  // const filterProductsByCategory = (category) => {
+  //   switch (category) {
+  //     case 'shirts':
+  //       return products.filter(product => product.title.toLowerCase().includes('shirt') || product.title.toLowerCase().includes('short sleeve'));
+  //     case 'pants':
+  //       return products.filter(product => product.title.toLowerCase().includes('pant') || product.title.toLowerCase().includes('jean'));
+  //     default:
+  //       return products.filter(product => 
+  //         !product.title.toLowerCase().includes('shirt') &&
+  //         !product.title.toLowerCase().includes('short sleeve') &&
+  //         !product.title.toLowerCase().includes('pant') &&
+  //         !product.title.toLowerCase().includes('jean')
+  //       );
+  //   }
+  // };
+
+  const onSwipedRight = (index) => {
+    setFavorites(prevFavorites => [...prevFavorites, products[index]]);
   };
 
-  const productsToDisplay = filterProductsByCategory(selectedCategory);
+  const onSwipedLeft = (index) => {
+    let updatedProducts = [...products];
+    const removedItem = updatedProducts.splice(index, 1);
+    updatedProducts.push(removedItem[0]);
+    setProducts(updatedProducts);
+  };
 
-  const CategoryButton = ({ title, onPress }) => (
-    <TouchableOpacity style={styles.optionButton} onPress={onPress}>
-      <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%" }}>
-        <Text style={styles.optionButtonText}>{title}</Text>
-        <AntDesign name="right" size={20} color="grey" />
-      </View>
-    </TouchableOpacity>
-  );
+  // const productsToDisplay = filterProductsByCategory(selectedCategory);
+
+  // const CategoryButton = ({ title, onPress }) => (
+  //   <TouchableOpacity style={styles.optionButton} onPress={onPress}>
+  //     <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%" }}>
+  //       <Text style={styles.optionButtonText}>{title}</Text>
+  //       <AntDesign name="right" size={20} color="grey" />
+  //     </View>
+  //   </TouchableOpacity>
+  // );
 
   return (
-    <ScrollView style={styles.container}>
-
-      <View style={styles.categories}>
-        <CategoryButton title="Shirts" onPress={() => setSelectedCategory('shirts')} />
-        <CategoryButton title="Pants" onPress={() => setSelectedCategory('pants')} />
-        <CategoryButton title="Other" onPress={() => setSelectedCategory('other')} />
-      </View>
-
-      {selectedCategory && productsToDisplay.map(product => (
-        <View key={product.id} style={styles.productContainer}>
-          <Image source={{ uri: product.image }} style={styles.productImage} />
-        </View>
-      )
-      )
-      }
-    </ScrollView>
+    <View style={styles.container}>
+      {products.length > 0 && (
+      <Swiper
+        cards={products}
+        renderCard={(card) => {
+          console.log(card);
+          return (
+            <View style={styles.card}>
+              <Image source={{ uri: card.image }} style={styles.cardImage} />
+              <Text style={styles.cardTitle}>{card.title}</Text>
+              <Text style={styles.cardPrice}>${card.price}</Text>
+            </View>
+          );
+        }}
+        onSwipedRight={onSwipedRight}
+        onSwipedLeft={onSwipedLeft}
+        infinite={true}  // for an infinite loop
+      />
+      )}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    backgroundColor: '#F5F5DC',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  text: {
-    fontSize: 18,
-    color: "#333",
+  card: {
+    marginTop: 20,
     marginBottom: 20,
-  },
-  categories: {
-    marginBottom: 20,
-  },
-  optionButton: {
-    padding: 20,
-    borderBottomWidth: 0.5,
-    borderBottomColor: "#ccc",
-  },
-  optionButtonText: {
-    fontSize: 18,
-    marginLeft: 10,
-  },
-  productContainer: {
-    marginBottom: 10,
-    padding: 10,
-    borderRadius: 8,
-    backgroundColor: "white",
+    width: '90%',
+    height: '60%',
+    borderRadius: 20,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
     },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
-  productImage: {
+  cardImage: {
     width: '100%',
-    height: 150,
-    resizeMode: 'contain',
-    borderRadius: 8,
+    height: '80%',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    resizeMode: 'cover',
+  },
+  cardTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  cardPrice: {
+    fontSize: 20,
+    marginBottom: 10,
   }
 }
 );
