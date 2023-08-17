9e40eb3dbc48d3c83b958bffd631d6ebeb0f33d8
rsori013 <rsori013@ucr.edu>
2023-08-17 14:11:53 -0700

    checkout button working to go to a new modal

--- screens/Favorites/CheckoutContainer.js
+++ screens/Favorites/CheckoutContainer.js
@@ -1,37 +1,55 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from "react-native";
 import SelectableCircle from "./SelectableCircle";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import OrderConfirmationModal from "./OrderConfirmationModal";
 
-const CheckoutContainer = ({ favorites, selectedItems, toggleAllSelection }) => {
-    const calculateTotal = () => {
-        return selectedItems.reduce((acc, itemId) => {
-          const item = favorites.find(fav => fav.id === itemId);
-          if (!item) {
-            console.warn(`Item with ID ${itemId} not found in favorites.`);
-            return acc;
-          }
-          // Remove the dollar sign and then parse the string to a float, tricky but goods now 
-          const price = parseFloat(item.price.replace('$', ''));
-          if (isNaN(price)) {
-            console.warn(`Price for item with ID ${itemId} is not a valid number after removing the dollar sign: ${item.price}`);
-            return acc;
-          }
-          return acc + price;
-        }, 0).toFixed(2);
-    };
-    
-    
+const CheckoutContainer = ({favorites,selectedItems,toggleAllSelection,}) => {
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const calculateTotal = () => {
+    return selectedItems
+      .reduce((acc, itemId) => {
+        const item = favorites.find((fav) => fav.id === itemId);
+        if (!item) {
+          console.warn(`Item with ID ${itemId} not found in favorites.`);
+          return acc;
+        }
+        // Remove the dollar sign and then parse the string to a float, tricky but goods now
+        const price = parseFloat(item.price.replace("$", ""));
+        if (isNaN(price)) {
+          console.warn(
+            `Price for item with ID ${itemId} is not a valid number after removing the dollar sign: ${item.price}`
+          );
+          return acc;
+        }
+        return acc + price;
+      }, 0)
+      .toFixed(2);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.selectAllContainer}>
-        <SelectableCircle 
-          isSelected={selectedItems.length === favorites.length} 
+        <SelectableCircle
+          isSelected={selectedItems.length === favorites.length}
           toggleSelection={toggleAllSelection}
         />
         <Text style={styles.selectAllText}>All</Text>
       </View>
-      <Text style={styles.checkoutText}>Total: ${calculateTotal()}</Text>
+      <View style={styles.totalContainer}>
+        <Text style={styles.totalText}>Total: ${calculateTotal()}</Text>
+      </View>
+      <View style={styles.CheckoutContainer}>
+        <TouchableOpacity style={styles.chekoutButton} 
+         onPress={() => setModalVisible(true)} >
+          <Text style={styles.checkoutText}>Checkout</Text>
+        </TouchableOpacity>
+        <OrderConfirmationModal
+          isVisible={modalVisible}
+          onClose={() => setModalVisible(false)}
+        />
+      </View>
     </View>
   );
 };
@@ -40,11 +58,11 @@ const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: 50,
-    backgroundColor: "red",
+    backgroundColor: "#f5f5f5",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
   },
   selectAllContainer: {
     flexDirection: "row",
@@ -54,9 +72,31 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     fontSize: 16,
   },
+  totalContainer: {
+    marginLeft: 90,
+  },
+  CheckoutContainer: {
+    marginRight: 10,
+    backgroundColor: "brown",
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  checkoutButton: {
+    fontSize: 15,
+    fontWeight: "400",
+    color: "white",
+  },
   checkoutText: {
-    fontSize: 18,
-    fontWeight: "bold",
+    fontSize: 15,
+    fontWeight: "400",
+    color: "white",
+  },
+  totalText: {
+    fontSize: 15,
+    fontWeight: "500",
   },
 });
 
