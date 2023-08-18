146e5bffbcac80e0ff3ba925ecdafb572174d948
rsori013 <rsori013@ucr.edu>
2023-08-18 13:47:06 -0700

    added place holders for order summary and prices

--- screens/Favorites/OrderConfirmationModal.js
+++ screens/Favorites/OrderConfirmationModal.js
@@ -43,79 +43,125 @@ const OrderConfirmationModal = ({ isVisible, onClose }) => {
       onRequestClose={onClose}
     >
       <ScrollView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <TouchableOpacity onPress={onClose} style={styles.leftArrow}>
-          <AntDesign name="arrowleft" size={35} color="black" />
-        </TouchableOpacity>
-        <Text style={styles.CheckoutText}>Checkout</Text>
-        <View style={styles.infoContainer}>
-          <Text style={styles.titleInfo}>My Information</Text>
-          <Text style={styles.infoText}>Name</Text>
-          <Text style={styles.infoText2}>Email</Text>
-          <TouchableOpacity onPress={handleEditInfo} style={styles.rightArrow}>
-            <AntDesign name="right" size={35} color="black" />
+        <View style={styles.container}>
+          <TouchableOpacity onPress={onClose} style={styles.leftArrow}>
+            <AntDesign name="arrowleft" size={35} color="black" />
           </TouchableOpacity>
+          <Text style={styles.CheckoutText}>Checkout</Text>
+          <View style={styles.infoContainer}>
+            <Text style={styles.titleInfo}>My Information</Text>
+            <Text style={styles.infoText}>Name</Text>
+            <Text style={styles.infoText2}>Email</Text>
+            <TouchableOpacity
+              onPress={handleEditInfo}
+              style={styles.rightArrow}
+            >
+              <AntDesign name="right" size={35} color="black" />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <Text style={styles.titleInfo}>Billing Address</Text>
+            <Text style={styles.infoText}>Name </Text>
+            <Text style={styles.infoText2}>Street Address </Text>
+            <Text style={styles.infoText2}>City</Text>
+            <Text style={styles.infoText2}>Zipcode</Text>
+            <Text style={styles.infoText2}>Country</Text>
+            <TouchableOpacity
+              onPress={handleBillingInfo}
+              style={styles.right2Arrow}
+            >
+              <AntDesign name="right" size={35} color="black" />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <Text style={styles.titleInfo}>Shipping</Text>
+            <Text style={styles.infoText}>Name </Text>
+            <Text style={styles.infoText2}>Phone number ### </Text>
+            <TouchableOpacity
+              onPress={handleShippingInfo}
+              style={styles.right3Arrow}
+            >
+              <AntDesign name="right" size={35} color="black" />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <Text style={styles.titleInfo}>Payment</Text>
+            <Text style={styles.infoText}>PamentcardOption </Text>
+            <Text style={styles.infoText2}>PamentcardOption </Text>
+
+            <TouchableOpacity
+              onPress={handlePaymentInfo}
+              style={styles.right4Arrow}
+            >
+              <AntDesign name="right" size={35} color="black" />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <View style={styles.packageContainer}>
+              <Text style={styles.titleInfo}>Package</Text>
+            </View>
+
+            <View style={styles.picturesContainer}>
+              <Text style={styles.titleInfo}>PICTURE HOLDER</Text>
+            </View>
+
+            <View style={styles.deliveryFreeContainer}>
+              <Text style={styles.titleInfo}>FREE 3-5 day shipping</Text>
+            </View>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <View style={styles.row}>
+              <Text style={styles.FeesText}>Discounts</Text>
+              <Text style={styles.valueText}>Apply Discount</Text>
+            </View>
+            <View style={styles.separator} />
+
+            <View style={styles.row}>
+              <Text style={styles.FeesText}>Order Value</Text>
+              <Text style={styles.valueText}>$79.99</Text>
+            </View>
+
+            <View style={styles.row}>
+              <Text style={styles.FeesText}>Delivery Fee</Text>
+              <Text style={styles.valueText}>FREE</Text>
+            </View>
+
+            <View style={styles.row}>
+              <Text style={styles.FeesText}>Est. taxes</Text>
+              <Text style={styles.valueText}>$9.99</Text>
+            </View>
+            <View style={styles.separator} />
+
+            <View style={styles.row}>
+              <Text style={styles.TotalText}>Total</Text>
+              <Text style={styles.valueText}>$ 999.99</Text>
+            </View>
+          </View>
         </View>
 
-        <View style={styles.infoContainer}>
-          <Text style={styles.titleInfo}>Billing Address</Text>
-          <Text style={styles.infoText}>Name </Text>
-          <Text style={styles.infoText2}>Street Address </Text>
-          <Text style={styles.infoText2}>City</Text>
-          <Text style={styles.infoText2}>Zipcode</Text>
-          <Text style={styles.infoText2}>Country</Text>
-          <TouchableOpacity
-            onPress={handleBillingInfo}
-            style={styles.right2Arrow}
-          >
-            <AntDesign name="right" size={35} color="black" />
-          </TouchableOpacity>
-        </View>
+        <UserInformation
+          isVisible={isUserModalVisible}
+          onClose={() => setUserModalVisible(false)}
+        />
 
-        <View style={styles.infoContainer}>
-          <Text style={styles.titleInfo}>Shipping</Text>
-          <Text style={styles.infoText}>Name </Text>
-          <Text style={styles.infoText2}>Phone number ### </Text>
-          <TouchableOpacity
-            onPress={handleShippingInfo}
-            style={styles.right3Arrow}
-          >
-            <AntDesign name="right" size={35} color="black" />
-          </TouchableOpacity>
-        </View>
+        <UserBilling
+          isVisible={isUserModalVisible2}
+          onClose={() => setUserModalVisible2(false)}
+        />
+        <UserShipping
+          isVisible={isUserModalVisible3}
+          onClose={() => setUserModalVisible3(false)}
+        />
 
-        <View style={styles.infoContainer}>
-          <Text style={styles.titleInfo}>Payment</Text>
-          <Text style={styles.infoText}>PamentcardOption </Text>
-          <Text style={styles.infoText2}>PamentcardOption </Text>
-
-          <TouchableOpacity
-            onPress={handlePaymentInfo}
-            style={styles.right4Arrow}
-          >
-            <AntDesign name="right" size={35} color="black" />
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      <UserInformation
-        isVisible={isUserModalVisible}
-        onClose={() => setUserModalVisible(false)}
-      />
-
-      <UserBilling
-        isVisible={isUserModalVisible2}
-        onClose={() => setUserModalVisible2(false)}
-      />
-      <UserShipping
-        isVisible={isUserModalVisible3}
-        onClose={() => setUserModalVisible3(false)}
-      />
-
-      <UserPayment
-        isVisible={isUserModalVisible4}
-        onClose={() => setUserModalVisible4(false)}
-      />
+        <UserPayment
+          isVisible={isUserModalVisible4}
+          onClose={() => setUserModalVisible4(false)}
+        />
       </ScrollView>
     </Modal>
   );
@@ -145,12 +191,11 @@ const styles = StyleSheet.create({
   titleInfo: {
     fontSize: 16,
     fontWeight: "600",
-    marginBottom: -30,
   },
   infoText: {
     fontSize: 14,
     fontWeight: "500",
-    marginTop: 40,
+    marginTop: 15,
   },
   infoText2: {
     fontSize: 14,
@@ -181,6 +226,39 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: 15,
     marginTop: 30,
+  },
+
+  packageContainer: {
+    flexDirection: "row",
+    padding: 20,
+    width: "100%",
+    borderWidth: 1,
+    marginTop: 20,
+  },
+  titlepackageContainer: {
+    marginTop: 20,
+  },
+
+  packageContainer: {
+    flex: 1,
+  },
+  picturesContainer: {
+    flex: 1,
+  },
+  deliveryFreeContainer: {
+    flex: 1,
+  },
+  separator: {
+    height: 2, // This determines the thickness of the line
+    backgroundColor: "black", // This is the color of the line
+    marginVertical: 10, // This adds some space above and below the line
+    width: "100%", // This ensures the line stretches across the container
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10, // Adds some space below each row
   },
 });
 
