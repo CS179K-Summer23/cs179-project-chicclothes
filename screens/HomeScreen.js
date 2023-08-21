be7bfab09fbb6fd6938c90e7ba82bc03e023e53f
jjdelcid <jgdelcid00@gmail.com>
2023-08-21 11:09:18 -0700

    new earluy design for homepage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,11 +12,17 @@ import {
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { Favorites } from "../data";
+import Carousel, { Pagination } from "react-native-snap-carousel";
 
 const HomeScreen = (navigation) => {
   const [products, setProducts] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
-  const [selectedProduct, setSelectedProduct] = useState({ item: null, index: null });
+  const [selectedProduct, setSelectedProduct] = useState({
+    item: null,
+    index: null,
+  });
+  const [activeSlide, setActiveSlide] = useState(0);
+  const [numColumns, setNumColumns] = useState(3);
 
   //   const handleHome = () => {
   //       navigation.navigate("Profile");
@@ -41,262 +47,176 @@ const HomeScreen = (navigation) => {
     setModalVisible(false);
   };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.titleContainer}>
-					<View style={[styles.smallContentTitle]}>
-						<Text style={styles.title}>Welcome</Text>
-					</View>
-				</View>
-      <View style={styles.welcomeContainer}>
-        <ScrollView
-          horizontal
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-          style={styles.imageScrollView}
-        >
-          <View style={styles.carouselSlide}>
-            <Image
-              source={require("./images/Clique_logo.png")}
-              style={styles.carouselImage}
-            />
-          </View>
-
-          <View style={styles.carouselSlide}>
-            {/* <TouchableOpacity onPress={handleHome}> */}
-            <Image
-              source={require("./images/group_picture.jpg")}
-              style={styles.carouselImage}
-            />
-            {/* </TouchableOpacity> */}
-            <Text style={styles.overlayText}>Join the Clique</Text>
-          </View>
-
-          <View style={styles.carouselSlide}>
-            <Image
-              source={require("./images/group_picture2.jpg")}
-              style={styles.carouselImage}
-            />
-            <Text style={styles.overlayText}>Checkout our Deals</Text>
-          </View>
-        </ScrollView>
-      </View>
-      <View style={styles.title2Container}>
-					<View style={[styles.smallContentTitle]}>
-						<Text style={styles.title}>Latest Exculsives</Text>
-					</View>
-				</View>
-      <View style={styles.productContainer}>
-        <View style={{ padding: 10 }}>
-          <FlatList
-            data={products}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item, index }) => (
-              <View style={styles.productBox}>
-              <View style={{ marginBottom: 20, marginHorizontal: 10}}>
-                <TouchableOpacity
-                  onPress={() => {
-                    setSelectedProduct({ item: item, index: index });
-                    setModalVisible(true);
-                  }}
-                >
-                  <Image
-                    source={{ uri: item.image }}
-                    style={{ width: 115, height: 95 }}
-                  />
-                    <Text style={{ fontWeight: "bold", textAlign: "center" , marginTop: 5}}>
-                    {item.price} USD
-                  </Text>
-                </TouchableOpacity>
-                </View>
-              </View>
-            )}
-            numColumns={2}
-          />
-          <Modal
-            animationType="slide"
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={() => {
-              setModalVisible(false);
-            }}
-          >
-            <View style={styles.centeredView}>
-              <View style={styles.modalView}>
-                <AntDesign
-                  name="arrowleft"
-                  size={30}
-                  color="black"
-                  position="absolute"
-                  left={10}
-                  onPress={() => setModalVisible(false)}
-                />
-                 <AntDesign
-                  name="hearto"
-                  size={30}
-                  color="red"
-                  style={styles.heartIcon}
-                  onPress={() => favoritePress(selectedProduct.index)}
-                />
-                {selectedProduct.item && (
-                  <>
-                    <Image
-                      source={{ uri: selectedProduct.item.image }}
-                      style={{ width: 250, height: 250 }}
-                    />
-                    <Text style={{ textAlign: "center" }}>
-                      {selectedProduct.item.title}
-                    </Text>
-                    <Text>{selectedProduct.item.description}</Text>
-                    <Text>{selectedProduct.item.price} USD</Text>
-                  </>
-                )}
-                <TouchableOpacity
-                  style={{ ...styles.button, backgroundColor: "#2196F3" }}
-                  onPress={() => setModalVisible(false)}
-                >
-                  <Text style={styles.textStyle}>Add to Cart</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </Modal>
-        </View>
+  const renderItem = ({ item }) => (
+    <View style={styles.slide}>
+      <Image source={item.uri} style={styles.image} />
+      <View style={styles.textOverlay}>
+        <Text style={styles.carouselText}>{item.text}</Text>
       </View>
     </View>
   );
+  const renderCategoryItem = ({ item }) => (
+    <View style={styles.categoryContainer}>
+    <View style={styles.categoryBox}>
+      <Image source={item.uri} style={styles.categoryImage} />
+    </View>
+    <Text style={styles.categoryText}>{item.name}</Text>
+  </View>
+  );
+  const categoriesData = [
+    { uri: require("./images/Clique_logo.png"), name: "Jackets" },
+    { uri: require("./images/Clique_logo.png"), name: "Shoes" },
+    { uri: require("./images/Clique_logo.png"), name: "Accessories" },
+    { uri: require("./images/Clique_logo.png"), name: "Jeans" },
+    { uri: require("./images/Clique_logo.png"), name: "Spanxs" },
+    { uri: require("./images/Clique_logo.png"), name: "Shirts" },
+    { uri: require("./images/Clique_logo.png"), name: "Hoodies" },
+    { uri: require("./images/Clique_logo.png"), name: "Dresses" },
+    { uri: require("./images/Clique_logo.png"), name: "Swimwear" },
+    { uri: require("./images/Clique_logo.png"), name: "T-shirts" },
+    { uri: require("./images/Clique_logo.png"), name: "Shorts" },
+    { uri: require("./images/Clique_logo.png"), name: "Bhorts" },
+  ];
+
+  const carouselItems = [
+    { uri: require("./images/Clique_logo.png"), text: "Text for Image 1" },
+    { uri: require("./images/group_picture.jpg"), text: "Text for Image 2" },
+    { uri: require("./images/group_picture2.jpg"), text: "Text for Image 3" },
+  ];
+
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.scrollViewContent}
+    >
+      <Carousel
+        data={carouselItems}
+        renderItem={renderItem}
+        sliderWidth={width}
+        itemWidth={width}
+        itemHeight={height * 0.75} // for example, half the screen height
+        sliderHeight={height * 0.75}
+        loop={true}
+        autoplay={true}
+        autoplayDelay={10000}
+        autoplayInterval={10000}
+        onSnapToItem={(index) => setActiveSlide(index)}
+      />
+      <Pagination
+        dotsLength={carouselItems.length}
+        activeDotIndex={activeSlide}
+        containerStyle={styles.paginationContainer}
+        dotStyle={styles.paginationDot}
+        dotColor="black"
+        inactiveDotColor="grey"
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+      />
+      <View style={styles.categoriesContainer}>
+        <Text style={styles.categoriesTitle}>Search By Category</Text>
+        <FlatList
+          key={numColumns} // This line forces a re-render when numColumns changes
+          data={categoriesData}
+          renderItem={renderCategoryItem}
+          keyExtractor={(item) => item.name}
+          numColumns={numColumns}
+          scrollEnabled={false}
+        />
+      </View>
+    </ScrollView>
+  );
 };
-const { width } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
-    height: "auto",
+    backgroundColor: "white",
   },
-  smallContentTitle: {
-    flex: 1,
-    flexDirection: "column",
-  },
-  titleContainer: {
-    height: "12%",
-    backgroundColor: "#e2ded3",
-    justifyContent: "flex-start",
-    flexDirection: "row",
-    marginBottom: -40,
-  },
-  title2Container: {
-    height: "12%",
-    backgroundColor: "#fff",
-    justifyContent: "flex-start",
-    flexDirection: "row",
-    marginTop: -40,
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: "bold",
-    color: "black",
-    marginLeft: 15,
-    top: 10,
-  },
-  welcomeContainer: {
-    width: "100%",
-    padding: 20,
-    backgroundColor: "#e2ded3", // or any other color you prefer
+  slide: {
+    height: height * 0.75, // Adjust as needed, keeping consistent with the Carousel height
+    width: width,
     justifyContent: "center",
     alignItems: "center",
-    height: 300,
+    backgroundColor: "white",
+  },
+  image: {
+    width: width,
+    height: height,
+    resizeMode: "cover",
+  },
+  paginationContainer: {
+    paddingVertical: 10,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  paginationDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 3,
+    backgroundColor: "rgba(255, 255, 255, 0.92)",
+  },
+  categoriesContainer: {
+    marginTop: 20,
+    padding: 15,
+  },
+  categoriesTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  textOverlay: {
+    position: "absolute",
+    bottom: 10, // for example, position text at the bottom of the image
+    left: 10,
+    right: 10, // ensure text doesn't overflow the image boundaries
+    alignItems: "center",
   },
 
-  productContainer: {
-    width: "100%",
-    height: 350,
-    backgroundColor: "#fff",
-    padding: 20,
-    bottom: 35,
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-    marginTop: 15,
-  },
-  textStyle: {
+  carouselText: {
+    fontSize: 18,
     color: "white",
     fontWeight: "bold",
     textAlign: "center",
+    backgroundColor: "rgba(0,0,0,0.5)", // optional, adds a semi-transparent background to make text more readable
+    padding: 5, // padding for the background
+    borderRadius: 5, // optional, round the corners of the background
   },
-  imageScrollView: {
-    width: width,
-  },
-  carouselImage: {
-    width: width,
-    height: 200,
-    resizeMode: "cover",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-  },
-  carouselSlide: {
-    width: width,
+  categoriesContainer: {
+    marginTop: 20,
+    padding: 15,
     alignItems: "center",
-    justifyContent: "center",
-    marginTop: -65,
   },
-  overlayText: {
-    position: "absolute",
-    color: "white",
-    fontSize: 24,
+
+  categoriesTitle: {
+    fontSize: 20,
     fontWeight: "bold",
+    marginBottom: 10,
     textAlign: "center",
-    textShadowColor: "black",
-    textShadowOffset: { width: -1, height: 1 },
-    textShadowRadius: 1,
   },
-  heartIcon: {
-    position: "absolute",
-    top: 10,
-    right: 10,
-    zIndex: 1,
-  },
-  productBox: {
-    borderColor: '#ccc',
-    borderWidth: 1,
-    padding: 5,
+
+  categoryBox: {
+    width: Dimensions.get("window").width / 3-50,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
     borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 3,
-    marginBottom: 20,
-    marginHorizontal: 10, // Give space between boxes horizontally
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 5,
+  },
+
+  categoryImage: {
+    width: 70, // adjust size as needed
+    height: 70, // adjust size as needed
+    borderRadius: 10,
+  },
+
+  categoryText: {
+    marginTop: 10,
+    fontSize: 12,
+    textAlign: "center",
   },
 });
 
